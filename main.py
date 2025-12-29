77e06277a637d31dd089da8aa3ad3283c0657aef
vinayakkam <87109462+vinayakkam@users.noreply.github.com>
2025-12-29 17:25:50 +0530

    Music Bot V6 patch

--- main.py
+++ main.py
@@ -8246,7 +8246,7 @@ async def on_ready():
         nodes = [
             wavelink.Node(
                 identifier="PRIMARY",
-                uri="wss://lavalinkv4.serenetia.com:443",
+                uri="wss://lava-v4.ajieblogs.eu.org:443",
                 password="https://dsc.gg/ajidevserver",
                 retries=3
             ),
